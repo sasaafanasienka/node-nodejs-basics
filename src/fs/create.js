901a2fc0e04532044817a878c6a410047ef7e1cc
Sasa Afanasienka <afanasienka@yandex.by>
2024-01-23 00:37:37 +0300

    init

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,44 @@
-const create = async () => {
-    // Write your code here 
-};
+import {writeFile, access} from 'node:fs/promises';
+import path from 'node:path';
 
-await create();
+class Creator {
+    constructor() {
+        this.path = 'src/fs/files/fresh.txt';
+        this.content = 'I am fresh and young'
+    }
+
+    fileExists = async () => {
+        return access(this.path).then(res => {
+            console.log('res', res)
+            return res
+        }).catch((err) => {
+            console.log('err', err)
+            return err
+        })
+    }
+
+    create = async () => {
+        this.fileExists().then(res => {
+            console.log(res)
+        })
+    
+    
+    
+        // const aaa = await fs.access(PATH)
+        // fs.realpath(PATH).then((res) => {
+        //     console.log(res)
+        // }).catch(err => {
+        //     console.log(err)
+        // })
+        // const file = await open(PATH).then(res => {
+        //     console.log(res)  
+        // }).catch((err) => {
+        //     console.log('Err', err)
+        // })
+        // Write your code here 
+    };
+
+}
+
+const creator = new Creator()
+creator.create();
