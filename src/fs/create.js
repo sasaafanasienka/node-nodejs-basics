ee72f982beee0b336d61571b7f9af7413e986f22
Sasa Afanasienka <afanasienka@yandex.by>
2024-01-23 00:53:14 +0300

    feat: first task

--- src/fs/create.js
+++ src/fs/create.js
@@ -3,39 +3,32 @@ import path from 'node:path';
 
 class Creator {
     constructor() {
-        this.path = 'src/fs/files/fresh.txt';
+        this.path = path.join('src/fs/files/fresh.txt');
         this.content = 'I am fresh and young'
     }
 
     fileExists = async () => {
-        return access(this.path).then(res => {
-            console.log('res', res)
-            return res
-        }).catch((err) => {
-            console.log('err', err)
-            return err
-        })
+        try {
+            await access(this.path)
+            return true
+        } catch (err) {
+            return false
+        }
     }
 
     create = async () => {
+        console.log(this.path)
         this.fileExists().then(res => {
-            console.log(res)
-        })
-    
-    
-    
-        // const aaa = await fs.access(PATH)
-        // fs.realpath(PATH).then((res) => {
-        //     console.log(res)
-        // }).catch(err => {
-        //     console.log(err)
-        // })
-        // const file = await open(PATH).then(res => {
-        //     console.log(res)  
-        // }).catch((err) => {
-        //     console.log('Err', err)
-        // })
-        // Write your code here 
+            if (res) {
+                throw Error('FS operation failed')
+            } else {
+                writeFile(this.path, this.content)
+                console.log('File has been written')
+            }
+        }).catch(error => {
+            console.error(error)
+            }
+        )
     };
 
 }
