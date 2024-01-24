520ea028fec44926223ebfe84ee352e399ab877b
Sasha Afanasienka <afanasienka@yandex.by>
2024-01-24 23:48:31 +0300

    feat: read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,4 +1,19 @@
+import {readFile} from 'node:fs/promises';
+import path from 'node:path';
+import {isPathExists} from '../helpers/helpers.js';
+
 const read = async () => {
+    const originPath = path.join('src/fs/files/fileToRead.txt');
+    
+    isPathExists(originPath).then(async res => {
+        if (res === false) {
+            throw Error('FS operation failed')
+        }
+        const content = await readFile(originPath);
+        console.log(content.toString())
+    }).catch((error) => {
+        console.error(error)
+    })
     // Write your code here 
 };
 
