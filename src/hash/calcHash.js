b795d9268985ac2a2339eeebbb3add020985f001
Sasha Afanasienka <afanasienka@yandex.by>
2024-01-25 22:45:31 +0300

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import path from 'node:path';
+import {createHash} from 'crypto';
+import { createReadStream } from 'node:fs';
+
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.join('src/hash/files/fileToCalculateHashFor.txt');
+
+    const readStream = createReadStream(filePath)
+    let result = ''
+    readStream.on('data', (data) => {
+        result = result + data
+    })
+    readStream.on('end', () => {
+        const hash = createHash('sha256')
+        const hashString = hash.update(result).digest('hex')
+        console.log(hashString)
+    })
 };
 
 await calculateHash();
